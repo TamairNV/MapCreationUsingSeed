6f72266d2290151c9caef6a7c3d5c8a518899805
Tamer Vassib <147373183+TamairNV@users.noreply.github.com>
2024-11-20 00:35:56 +0000

    Boiler plate

--- MapCreationUsingSeed/Program.cs
+++ MapCreationUsingSeed/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using System.Numerics;
+using Raylib_cs;
+using System.IO;
+using System.Text.Json;
 
-Console.WriteLine("Hello, World!");
+
+public class Program
+{
+    public static void Main()
+    {
+        Raylib.InitWindow(950, 751, "Map Creation");
+        Raylib.SetTargetFPS(300);
+        while (!Raylib.WindowShouldClose())
+        {
+            
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
+        
+
+    }
+}
