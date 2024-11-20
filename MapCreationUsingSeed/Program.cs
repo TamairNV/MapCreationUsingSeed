14e6602542b5fc519aca8e6fb331eeaa6a7b4508
Tamer Vassib <147373183+TamairNV@users.noreply.github.com>
2024-11-20 00:59:40 +0000

    added quick displays

--- MapCreationUsingSeed/Program.cs
+++ MapCreationUsingSeed/Program.cs
@@ -3,6 +3,7 @@ using System.Numerics;
 using Raylib_cs;
 using System.IO;
 using System.Text.Json;
+using MapCreationUsingSeed;
 
 
 public class Program
@@ -11,9 +12,25 @@ public class Program
     {
         Raylib.InitWindow(950, 751, "Map Creation");
         Raylib.SetTargetFPS(300);
+
+
+        int[,] g = new int[,]
+        {
+            {0,0,0,0,0},
+            {0,1,1,1,1},
+            {0,0,1,1,0},
+            {0,0,1,0,0},
+            {0,0,0,0,0}
+        };
+
+        QuickDisplay test = new QuickDisplay(100, 100, 10, g, Color.Orange);
         while (!Raylib.WindowShouldClose())
         {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.Blue);
+  
             
+            QuickDisplay.DrawDisplays();
             Raylib.EndDrawing();
         }
         Raylib.CloseWindow();
---
+++ MapCreationUsingSeed/QuickDisplay.cs
@@ -0,0 +1,97 @@
+namespace MapCreationUsingSeed;
+using System.Numerics;
+using Raylib_cs;
+public class QuickDisplay
+{
+    public static List<QuickDisplay> QuickDisplays = new List<QuickDisplay>();
+    public Rectangle display;
+    private Color colour;
+    
+    public Vector2 gridSize;
+    public int[,] grid;
+    private List<Rectangle> lines = new List<Rectangle>();
+    private List<Rectangle> pixels = new List<Rectangle>();
+    public int cellSize;
+    public QuickDisplay(int X, int Y, int cellSize, int[,] grid,Color colour)
+    {
+        QuickDisplays.Add(this);
+        int width = cellSize * grid.GetLength(0);
+        int height = cellSize * grid.GetLength(1);
+        display = new Rectangle(X, Y, width, height);
+        this.colour = colour;
+        gridSize = new Vector2(width, height);
+        this.grid = grid;
+        this.cellSize = cellSize;
+
+        createGridLines();
+        findPixels();
+
+    }
+
+    public void DrawDisplay()
+    {
+        Raylib.DrawRectangleRec(display, colour);
+        drawGridLines(Color.Black);
+        drawPixels();
+
+
+    }
+    private void createGridLines(int lineThickness = 2)
+    {
+        for (int x = 0; x <= gridSize.X/cellSize; x++)
+        {
+            Rectangle rect = new Rectangle(x * cellSize + display.X, + display.Y, lineThickness, gridSize.Y+lineThickness);
+            lines.Add(rect);
+        }
+        for (int x = 0; x <= gridSize.Y/cellSize; x++)
+        {
+            Rectangle rect = new Rectangle(+ display.X,x*cellSize+ display.Y,gridSize.X,lineThickness);
+            lines.Add(rect);
+        }
+        
+    }
+
+    private void drawPixels()
+    {
+        foreach (var pixel in pixels)
+        {
+            Raylib.DrawRectangleRec(pixel,Color.Black);
+        }
+    }
+
+    private void findPixels()
+    {
+        for (int x = 0; x < gridSize.X/cellSize; x++)
+        {
+            for (int y = 0; y < gridSize.Y/cellSize; y++)
+            {
+                if (grid[y,x] != 0)
+                {
+                    Rectangle rect = new Rectangle(x * cellSize + display.X, y * cellSize + display.Y, cellSize,
+                        cellSize);
+                    
+                    pixels.Add(rect);
+                }
+            }
+        }
+    }
+    
+    
+
+    public void drawGridLines(Color colour)
+    {
+        foreach (var line in lines)
+        {
+            Raylib.DrawRectangleRec(line,colour);
+        }
+        
+    }
+
+    public static void DrawDisplays()
+    {
+        foreach (var display in QuickDisplays)
+        {
+            display.DrawDisplay();
+        }
+    }
+}
